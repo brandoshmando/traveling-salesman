3f8c7b7b1019e819677307dca5de74a3634af8d5
brandoshmando <brancraft@gmail.com>
2014-07-07 18:47:35 -0400

    Changed some styling and added submit/add destination button. Needs a ton more styling but yea...

--- public/main.js
+++ public/main.js
@@ -9,3 +9,9 @@ function initialize() {
   var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+
+//Add destination input
+
+$( 'document' ).ready(funtcion{
+  $('')
+});
