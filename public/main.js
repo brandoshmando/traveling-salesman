ec3a6574b3d9cc13170ad8bf75e49107961a5690
brandoshmando <brancraft@gmail.com>
2014-07-07 22:47:54 -0400

    Added delete button to remove extra destination buttons

--- public/main.js
+++ public/main.js
@@ -3,7 +3,7 @@
 function initialize() {
   var mapOptions = {
     center: new google.maps.LatLng(43.7182712, -79.3777061),
-    zoom: 12
+    zoom: 10
   };
 
   var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
@@ -20,14 +20,26 @@ $( 'document' ).ready(function(){
   });
 });
 
+
+//Delete destination input
+$( 'document' ).ready(function(){
+  $('#del-btn').click(function(){
+    var destInputs = $('.address-input').not("#submit-button, #del-btn, #add-btn");
+    if (destInputs.length > 2){
+      destInputs.last().remove();
+    };
+  });
+});
 //Some food for thought:
 /* In console, this works to recieve value of #start-pt input:
 var aValue = $('#start-pt').val();
 
-However this does not:
+However this does not:*/
 
-$('#submit-button').click(function(){
-var input = $('#start-pt')
-var bValue = input.val();
-return false;
-})
+// $('#submit-button').click(function(){
+// var input = $('#start-pt');
+// var bValue = input.val();
+// console.log(bValue);
+// console.log("Function done...")
+// return false;
+// })
